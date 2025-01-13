1f6ee99b80760fdf94cac01209fa2883903f8527
ablokhin1991 <a.blokhin1991@gmail.com>
2025-01-13 17:53:57 +0300

    Обновление всех изменений

--- css/application.js
+++ css/application.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const params = new URLSearchParams(window.location.search);
-
-    // Заполнение значений на странице заявки
-    document.getElementById("guarantee-sum").value = params.get("guaranteeSum") || "Не указано";
-    document.getElementById("guarantee-days").value = params.get("guaranteeDays") || "Не указано";
-    document.getElementById("procedure-type").value = params.get("procedureType") || "Не указано";
-    document.getElementById("guarantee-type").value = params.get("guaranteeType") || "Не указано";
-
+  
+    // Заполнение полей формы
+    document.querySelector(".text_light").textContent = params.get("bankName") || "Не указано";
+    document.getElementById("guarantee-sum").value = params.get("guaranteeSum") || "";
+    document.getElementById("guarantee-days").value = params.get("guaranteeDays") || "";
+    document.getElementById("procedure-type").value = params.get("procedureType") || "";
+    document.getElementById("guarantee-type").value = params.get("guaranteeType") || "";
+  
     // Обработка чекбоксов
     document.getElementById("has-advance").checked = params.get("hasAdvance") === "Да";
     document.getElementById("custom-form").checked = params.get("customForm") === "Да";
-});
+  });
+  
 
