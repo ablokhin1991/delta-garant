0a7991500a3c7165ff4bebe1a8acec0b5d55944f
ablokhin1991 <a.blokhin1991@gmail.com>
2025-01-15 01:04:41 +0300

    Обновление всех изменений

--- css/virtual-page.js
+++ css/virtual-page.js
@@ -105,12 +105,14 @@ function openVirtualPage(offer, selectedParams) {
                     <div class="content-vp">
                         <h2>Выбранные параметры</h2>
                         <div class="form-row">
+                            <div class="form-column">
                             <div class="upload-row">
                                 <p>Сумма гарантии: ${formattedSum}</p>
                             </div>
                             <div class="upload-row">
                                 <p>Срок действия: ${selectedParams.days} дней</p>
                             </div>
+                        </div>
                             <div class="form-column">
                             <div class="upload-row">
                                 <p>Тип процедуры: ${getProcedureTypeName(selectedParams.procType)}</p>
