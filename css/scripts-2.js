ed12c86c308d6c928e0076a98d7512b6ae84ec71
ablokhin1991 <a.blokhin1991@gmail.com>
2025-01-17 00:26:23 +0300

    Обновление всех изменений

--- css/scripts-2.js
+++ css/scripts-2.js
@@ -450,7 +450,6 @@ offerList.innerHTML = finalResults
         <div class="offer__buttons">
           ${!result.isStopFactor ? `
             <button class="btn_primary offer__button" data-index="${index}">Оформить</button>
-            <button class="btn_secondary offer__button btn_request" data-index="${index}" onclick="window.location.href='ссылка_для_заявки';">Заявка в 1 клик</button>
           ` : ''}
         </div>
     </div>`
