c7f9c3164a26d908a5a517a267fb7d977b29fe19
ablokhin1991 <a.blokhin1991@gmail.com>
2025-01-19 00:45:14 +0300

    Обновление всех изменений

--- css/scripts-2.js
+++ css/scripts-2.js
@@ -746,7 +746,7 @@ offerList.innerHTML = finalResults
   <span class="button-82-shadow"></span>
   <span class="button-82-edge"></span>
   <span class="button-82-front text">
-    Button 82
+    Оформить
   </span>
 </button>
     </div>`
