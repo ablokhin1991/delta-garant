29f4286e22517cbd74c6d97c4d6ef8e32543004a
ablokhin1991 <a.blokhin1991@gmail.com>
2025-02-02 21:56:11 +0300

    Обновление всех изменений

---
+++ css/o-nas.js
@@ -0,0 +1,28 @@
+// Скрипты для страницы "О нас"
+
+// Модальное окно для отзывов
+function openModal(imgSrc) {
+    document.getElementById('modalImage').src = imgSrc;
+    document.getElementById('reviewModal').style.display = 'block';
+  }
+  
+  function closeModal() {
+    document.getElementById('reviewModal').style.display = 'none';
+  }
+  
+  // Закрытие при клике вне изображения
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('reviewModal')) {
+      closeModal();
+    }
+  }
+  
+  // Плавная прокрутка для якорей
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
