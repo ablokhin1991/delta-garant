aa0831f513d58a930ad14bd0fe52fd40adfd7888
ablokhin1991 <a.blokhin1991@gmail.com>
2025-02-03 17:21:06 +0300

    Обновление всех изменений

--- css/o-nas.js
+++ css/o-nas.js
@@ -18,11 +18,11 @@ function openModal(imgSrc) {
   }
   
   // Плавная прокрутка для якорей
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
+  //document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+   // anchor.addEventListener('click', function(e) {
+    //  e.preventDefault();
+    //  document.querySelector(this.getAttribute('href')).scrollIntoView({
+     //   behavior: 'smooth'
+      //});
+   // });//
+  //});
