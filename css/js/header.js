d4d45fbf39da1929c1fd3880837414481fc4f05a
ablokhin1991 <a.blokhin1991@gmail.com>
2025-02-13 14:12:53 +0300

    Обновление всех изменений

--- css/js/header.js
+++ css/js/header.js
@@ -1,8 +1,15 @@
 // Скрипт для переключения мобильного меню
-    document.addEventListener("DOMContentLoaded", function() {
-      var burgerMenu = document.querySelector('.burger-menu');
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("header.js загружен");
+    var burgerMenu = document.querySelector('.burger-menu');
+    if (burgerMenu) {
       burgerMenu.addEventListener("click", function() {
+        console.log("Нажата бургер-иконка");
         var navMenu = document.querySelector('.nav-menu');
         navMenu.classList.toggle('active');
       });
-    });
+    } else {
+      console.error("Элемент .burger-menu не найден");
+    }
+  });
+  
