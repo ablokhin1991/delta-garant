27a5d9c9b24acee6175a1514e589db4309493284
ablokhin1991 <a.blokhin1991@gmail.com>
2025-05-23 17:20:04 +0300

    Обновление всех изменений

--- css/js/services-lottie.js
+++ css/js/services-lottie.js
@@ -4,7 +4,7 @@
 const services = [
   {
     id: 'lottie1',
-    path: 'css/json/animated-icons/wired-flat-1780-medal-first-place-hover-pinch.json'
+    path: 'css/json/animated-icons/Red-and-blue-medal.json'
   },
   {
     id: 'lottie2',
